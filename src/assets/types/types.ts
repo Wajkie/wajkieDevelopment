c654d2e97bb6845b836d561ad95ee204c3b8faca
Fredrik Wiking <wikingfredrik@gmail.com>
2024-12-23 09:22:39 +0100

    rewriting the weatherapp to be integrated in the showcase

---
+++ src/assets/types/types.ts
@@ -0,0 +1,39 @@
+export interface WeatherApp{
+coord: {
+      lon: number,
+      lat: number,
+   },
+   weather: [
+      {
+         id: number,
+         main: string,
+         description: string,
+         icon: string
+      }
+   ],
+   main: {
+      temp: number,
+      feels_like: number,
+      temp_min: number,
+      temp_max: number,
+   },
+
+   wind: {
+      speed: number,
+      deg: number,
+      gust: number
+   },
+   clouds: {
+      all: number
+   },
+   dt: number,
+   sys: {
+      type: number,
+      id: number,
+      country: string,
+      sunrise: number,
+      sunset: number
+   },
+   timezone: number,
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-import "./assets/style/app.scss"
+import "./assets/style/app.scss";
+import { db} from "./assets/firebase/firebase";
+const appEl = document.querySelector<HTMLDivElement>("#app")!;
+appEl.innerHTML= `
+	<header>
+		<h1> Welcome to Wajkie Development!<h1>
+	</header>
+	<section>
+		<h2></h2>
+	</section>
+	<footer>
+		&copy Wajkie - Fredrik Wiking
+		<hr>
+		<a href="https://github.com/Wajkie" target="blank">Wajkie on github</a>
+	</footer>
+
+`
